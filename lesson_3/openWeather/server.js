14e5c514a790a2110dc9f7fef9384781c9fc9c8d
zoedb <zoedberg@i.ua>
2020-07-16 13:05:31 +0300

    added proxy server for openWeather API

--- lesson_3/openWeather/server.js
+++ lesson_3/openWeather/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 
 const { weatherRouter } = require('./src/routes');
 
@@ -10,8 +10,17 @@ dotenv.config();
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
 
+const ownCors = (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Method", "GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    next();
+}
+
+app.use(ownCors);
 
 app.use('/weather', weatherRouter);
 
