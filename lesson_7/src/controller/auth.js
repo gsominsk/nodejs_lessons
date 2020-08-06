b5e7117907a10a43223a3f8b274aa1316c18c3ed
zoedb <zoedberg@i.ua>
2020-08-06 03:38:26 +0300

    added jwt token

--- lesson_7/src/controller/auth.js
+++ lesson_7/src/controller/auth.js
@@ -7,16 +7,21 @@ const auth = {
 
         if (!login || !password) return throwErr(400, 'Check required fields.');
 
+        // check user exists
         const user = await userModel.findOne({ login });
-
         if (!user) return throwErr(404, 'User not found.');
 
+        // check password correct
         const isValid = await user.comparePassword(password.toString());
-
         if (!isValid) return throwErr(400, 'Password dont match.');
+
+        // check password generate new token and update user in db
+        const token = await user.generateToken();
+        const userWithToken = await userModel.findOneAndUpdate({ _id: user._id }, { $set: { token } });
 
         return {
             status: 'ok',
+            token: userWithToken.token,
         }
     },
 
--- lesson_7/src/db/models/user.js
+++ lesson_7/src/db/models/user.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
     login: mongoose.Schema.Types.String,
     password: mongoose.Schema.Types.String,
+    token: {
+        type: mongoose.Schema.Types.String,
+        default: null,
+    },
     tasks: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'task',
@@ -28,6 +34,25 @@ userSchema.pre('save', async function(next)  {
 
 userSchema.methods.comparePassword = function(candidatePassword) {
     return bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.methods.generateToken = function() {
+    const user = this;
+    const { JWT_SECRET_TOKEN } = process.env
+
+    return jwt.sign({ _id: user._id }, JWT_SECRET_TOKEN);
+};
+
+userSchema.methods.isValidToken = function(token) {
+    const { JWT_SECRET_TOKEN } = process.env
+
+    try {
+        jwt.verify(token, JWT_SECRET_TOKEN);
+    } catch(err) {
+        return false;
+    }
+
+    return true;
 };
 
 const User = mongoose.model('user', userSchema);
