578df659b64a8efe2bafd9ac21c7075f723b5b29
zoedb <zoedberg@i.ua>
2020-08-17 23:08:39 +0300

    added lesson 10
    added example of mock with sinon

---
+++ lesson_10/__tests__/unit/index.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const getAlbumById = async function(id) {
+    const requestUrl = `https://jsonplaceholder.typicode.com/albums/${id}/photos?_limit=3`;
+    return new Promise((resolve, reject) => {
+        request.get(requestUrl, (err, res, body) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(JSON.parse(body));
+        });
+    });
+}
+
+module.exports = {
+    getAlbumById,
+};
